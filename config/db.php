effbcff2463b7aaab95e3f315d657e942a746455
flavio-pessoa <flavioluis.for@gmail.com>
2019-10-22 13:59:13 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=demo_db',
     'username' => 'demo_user',
-    'password' => '12345678',
+    'password' => '!Q@W#E$R1q2w3e4r',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
